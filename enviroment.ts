32adc403e493a0e60cf7b5b63944472236cd646d
Gabriel Zanatto Salami <gabriel.zanatto2@gmail.com>
2020-09-14 15:41:02 -0300

    Delete enviroment.ts

--- enviroment.ts
+++
@@ -1,5 +0,0 @@
-const secret = process.env.SECRET || 't5b3b9a5';
-const mongoUrl = process.env.MONGOURL || 'mongodb+srv://salami1996:salami1996@cluster0-utam0.gcp.mongodb.net/produtos-app?retryWrites=true&w=majority';
-const port = process.env.PORT || 9000;
-
-export { secret, mongoUrl, port };
