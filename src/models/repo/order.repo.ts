c054192bc742665143bd3b821f112682e67a0cd7
Gabriel Zanatto Salami <gabriel.zanatto2@gmail.com>
2021-05-06 10:17:31 -0300

    fix: using nested populate in mongoDb queries

--- src/models/repo/order.repo.ts
+++ src/models/repo/order.repo.ts
@@ -1,6 +1,19 @@
 import { Order } from "../entities";
 import { OrderModel } from "../schemas";
 
+const populate = [{
+    path: 'client',
+    populate: {
+        path: 'address'
+    }
+}, {
+    path: 'address'
+}, {
+    path: 'payment'
+}, {
+    path: 'store'
+}]
+
 export class OrderRepository {
     static async saveOrder(o: Order): Promise<Order> {
         const orders = await OrderModel.find({ store: o.store }).sort('cod').exec();
@@ -12,15 +25,15 @@ export class OrderRepository {
         return await OrderModel.create(o);
     }
     static async getOrders(store: string): Promise<Order[]> {
-        const resp = await OrderModel.find({ store }).populate('client payment address').exec();
+        const resp = await OrderModel.find({ store }).populate(populate).exec();
         return resp;
     }
     static async getOrdersByUser(store: string, client: string): Promise<Order[]> {
-        const resp = await OrderModel.find({ $and: [{ store }, { client }] }).populate('store payment address').exec();
+        const resp = await OrderModel.find({ $and: [{ store }, { client }] }).populate(populate).exec();
         return resp;
     }
     static async getOrder(_id: string): Promise<Order> {
-        const order = await OrderModel.findOne({ _id }).populate('store payment client address').exec();
+        const order = await OrderModel.findOne({ _id }).populate(populate).exec();
         return order;
     }
     static async updateOrder(_id: string, status: number): Promise<Order> {
